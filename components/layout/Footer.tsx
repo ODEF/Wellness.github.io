44f6cb70c5a1784e0669619f72dfc77f8ebc330f
ODEF <103530614+ODEF@users.noreply.github.com>
2026-06-28 13:42:10 +0400

    stoped onedrive

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -14,7 +14,7 @@ export default function Footer() {
         */}
 
         <div className="right flex w-full flex-col items-center gap-y-6 md:flex-row md:justify-between md:gap-y-0">
-          <Image src="../../public/assets/wellness-logo.jpeg" alt="Healthy Paw Logo" className="h-12 w-auto" />
+          <Image src="/assets/wellness-logo.jpeg" alt="Healthy Paw Logo" className="h-12 w-auto" />
           <p className="text-sm text-blue-gray-500">
             &copy; 2024 Healthy Paw. All rights reserved.
           </p>
